96df48447d5d1ac97376f9f986565e2c8fbae8b7
Chris Mague <github@mague.com>
2021-06-29 09:11:48 -0700

    rewrite to use both the index and the query sha to avoid any collisions

--- gears/esSearch.py
+++ gears/esSearch.py
@@ -7,20 +7,23 @@ import json
 # Returns JSON
 
 def fetch_and_load(sha, index, query, ttl):
-    es = Elasticsearch(hosts="es")
-    res = es.search(index=index, body=query)
-    execute("SETEX", sha, ttl, json.dumps(res))
-    return(res)
+    try:
+        es = Elasticsearch(hosts="es")
+        res = es.search(index=index, body=query)
+        execute("SETEX", "{}:{}".format(index, sha), ttl, json.dumps(res))
+        return(res, 0)
+    except Exception as res:
+        return(res.error, 1)
 
 def runIt(x):
-    w = execute("GET", x[1])
+    w = execute("GET", "{}:{}".format(x[2],x[1]))
     if w :
-        t = execute("TTL", x[1])
+        t = execute("TTL", "{}:{}".format(x[2],x[1]))
         j = json.loads(w)
         out = {"result": j, "ttl": t, "exit_code": 0}
     else:
-        res = fetch_and_load(x[1], x[2], x[3], x[4])
-        out = {"result": res, "ttl": x[4], "exit_code": 0}
+        res, exit_code = fetch_and_load(x[1], x[2], x[3], x[4])
+        out = {"result": res, "ttl": x[4], "exit_code": exit_code}
 
     return(json.dumps(out))
 
